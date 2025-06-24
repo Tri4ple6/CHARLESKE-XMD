76acf677a5a9259b3057cfdc092dcc9328e70fb3
Tri4ple6 <wenslauswanyonyi59@gmail.com>
2025-06-24 13:00:41 +0300

    Update talkdrove.js

--- talkdrove.js
+++ talkdrove.js
@@ -8,7 +8,7 @@
     "owner-verification": "Charlesodhiamboofficiall@gmail.com",  
     "env": {
       "SESSION_ID": {
-        "description": "Put your SESSION_ID here. Make sure it starts with ey~  or get session id from here,  https://charle-ke.onrender.com/",
+        "description": "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK1BIcUtUV1FCdnY5b3EyTTd2cVBFU0RhVHJiWUZnUzRQZ1RRa3dLTFVXST0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMzN0VUJhTVRnTEVCRFpPeHhBRm0vME9DaGh6UlZOYXFBQk1LaUtoYzlCRT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJzQjV2Tk1vbjhML2YyaTRSVWxEV0V5d0RUbWx3TTJRaVNVdVZkV1AzODA4PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJDQU5xQnljd3hJZkp6ZG9INVdsK09DSnVBWGhTRXovd01HbVZTZm5TcUE0PSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IklJdmRiRHFHb1lPY2lQOXRnUSt0WHhWeWdrNjdzb05SS3QvTnZDZmxKbUE9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjBndGJSbHV1S1E4Tm0wd2dXbnZUYTBabjJSQ1hQL25XNEV2Mm9UWUNNVDg9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYUYvT1hUaDZZRDZnekw0Z2FSRG1YYTNhREVUOTRxVFFpc2RrK056MkZrcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidlNOTXVYM0hna0dsZHRiVmJQK2t4N21tVUJXd3hCOUZORkV6TTNDMjdDVT0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjN6QnRTQXhZdHRqUkhiQTRXbHBRY2ZsQVVIYWs4Y2JYUjYvUmgzTXRrMVRKVnU1bHUzRGtqcnpWdm1LeHBYNnk4eHFEMjdtRmFHZmdPTFBZWmRNN2d3PT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTQ1LCJhZHZTZWNyZXRLZXkiOiJFRGtLSG13M0JObkdRMzNXWE1yRnJBTHc4cU5laEtqa0pCZE1mUWNsUlhjPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W3sia2V5Ijp7InJlbW90ZUppZCI6IjI1NDcxMDgzMzY3NkBzLndoYXRzYXBwLm5ldCIsImZyb21NZSI6dHJ1ZSwiaWQiOiI1MjVDQkQwREQ3Q0Y3RUE2NUVFOTg3MDY0NjQ4Mzc3NiJ9LCJtZXNzYWdlVGltZXN0YW1wIjoxNzUwNzU4NzQxfSx7ImtleSI6eyJyZW1vdGVKaWQiOiIyNTQ3MTA4MzM2NzZAcy53aGF0c2FwcC5uZXQiLCJmcm9tTWUiOnRydWUsImlkIjoiNjM5MkQwQ0ZGQ0NCQkJFOUYwRkE0M0JBMjMyM0M4RTQifSwibWVzc2FnZVRpbWVzdGFtcCI6MTc1MDc1ODc0NH1dLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTeW5jQ291bnRlciI6MSwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sInJlZ2lzdGVyZWQiOnRydWUsInBhaXJpbmdDb2RlIjoiMTIzTE9UVVMiLCJtZSI6eyJpZCI6IjI1NDcxMDgzMzY3NjoxQHMud2hhdHNhcHAubmV0IiwibGlkIjoiMTQxNjM1NjkwMjI5NzcyOjFAbGlkIiwibmFtZSI6IldlTlNMQVVTIFRSaXBQTEXihKIifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ015RGs0MEZFTURxNmNJR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IjFDdmE3YXYzYWpjSTNSVkJZSGQxNWtaaktqRkt0YU84NUd6ckd5MGYvUnc9IiwiYWNjb3VudFNpZ25hdHVyZSI6ImtNZHozOUtXekZkSVJrYmttL3VScFZ1RjBJUEdmL2kxVUNVY2hYaXFzZWRWZkhiUzNCOWdkYU5WQlhtZ0ZyVE04Z0NiczllRXptaVZnZ2hiQmg0c0J3PT0iLCJkZXZpY2VTaWduYXR1cmUiOiJnZkJSZ0I0VjcwN1Y1S3dGVWxZODBLQ0hKVFZFQWhnUU9kS20ySktPRzl0YlI5aEJpRVJJcFlDLytHVXZTTGtvTFVIb0htejQrU1Z0Q0dzYkMvbjNnUT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjI1NDcxMDgzMzY3NjoxQHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQmRRcjJ1MnI5Mm8zQ04wVlFXQjNkZVpHWXlveFNyV2p2T1JzNnhzdEgvMGMifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJyb3V0aW5nSW5mbyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkNBMElDQT09In0sImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTc1MDc1ODczNCwibGFzdFByb3BIYXNoIjoiMUs0aEg0IiwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFEelUifQ==",
         "value":"",
         "required": true        
     },
@@ -28,7 +28,7 @@
       "value": "public"      
     },        
     "ALWAYS_ONLINE": {
-      "description": "Make it true if want always online.",
+      "description": "true.",
       "required": false,
       "value": "false"  
    },      
@@ -58,12 +58,12 @@
       "value": "false"   
    },     
     "OWNER_NAME": {
-      "description": "Type Bot Owner Name.",
+      "description": "wenslaus.",
       "required": false,
-      "value": "®charleske"
+      "value": "@charlseke"
    }, 
     "OWNER_NUMBER": {
-      "description": "put the owner number for bot.",
+      "description": "254710833676.",
       "required": false,
       "value": "254759626063"
    }, 
